29413692b8140b9b2b1b596c247e6cc8a19ba396
Marcelo Tavares <marcelonascimento.contato@gmail.com>
2025-09-27 02:34:56 -0300

    teste

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('🚀 Testando deploy com Argo CD');
+  res.send('🚀 Testando deploy com ArgoCD !! ');
 });
 
 app.listen(PORT, () => {
